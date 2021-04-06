dc277162cd24874c8d9c03d3379a6e592e0468ed
gvisda <60252265+gvisda@users.noreply.github.com>
2021-04-06 18:38:49 -0500

    Update scales.js

--- scales.js
+++ scales.js
@@ -1,1 +1,6 @@
 const AMinorScale = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+const addOctaveNumbers = (scale, octaveNumber) => scale.map(note => {
+  const firstOctaveNoteIndex = scale.indexOf('C') !== -1 ? scale.indexOf('C') : scale.indexOf('C#')
+  const noteOctaveNumber = scale.indexOf(note) < firstOctaveNoteIndex ? octaveNumber - 1 : octaveNumber;
+  return `${note}${noteOctaveNumber}`
+});
